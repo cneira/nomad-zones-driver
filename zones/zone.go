84526bfc45df3ce202a732a93f2d2c6e63e6c99d
cneira <cneirabustos@gmail.com>
2019-05-16 13:42:25 -0400

    clean up

--- zones/zone.go
+++ zones/zone.go
@@ -2,7 +2,7 @@ package zone
 
 import (
 	"fmt"
-//	"strings"
+
 	"git.wegmueller.it/illumos/go-zone/config"
 	"github.com/hashicorp/nomad/plugins/drivers"
 )
@@ -13,17 +13,8 @@ func (d *Driver) initializeContainer(cfg *drivers.TaskConfig, taskConfig TaskCon
 	z := config.New(containerName)
 	z.Autoboot = false
 	z.Brand = "pkgsrc"
-
 	z.Zonepath = fmt.Sprintf("/zcage/vms/%s",containerName)
-
-        // validzpath := strings.Replace(containerName,"-","",-1)
 	
-	// if len(validzpath) > 32 {
-	
-	// } else { 
-    	// z.Zonepath = fmt.Sprintf("/zcage/vms/%s",validzpath)	
-        // }
-
 	return z
 
 }
