e79aac4c385b74e706199a822842a6110afdbd5a
cneira <cneirabustos@gmail.com>
2019-06-07 20:05:13 -0400

    fix: docker pull now gets all layers

--- zones/zone.go
+++ zones/zone.go
@@ -1,6 +1,8 @@
 package zone
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"crypto/rand"
 	"encoding/json"
 	"fmt"
@@ -8,8 +10,11 @@ import (
 	hclog "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/nomad/plugins/drivers"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -26,6 +31,174 @@ func simple_uuid() (string, error) {
 	}
 	uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 	return uuid, nil
+}
+
+func tarit(source, target string) error {
+	filename := filepath.Base(source)
+	target = filepath.Join(target, fmt.Sprintf("%s.tar", filename))
+	tarfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer tarfile.Close()
+
+	tarball := tar.NewWriter(tarfile)
+	defer tarball.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	return filepath.Walk(source,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			header, err := tar.FileInfoHeader(info, info.Name())
+			if err != nil {
+				return err
+			}
+
+			if baseDir != "" {
+				header.Name = filepath.Join(".", strings.TrimPrefix(path, source))
+			}
+
+			if err := tarball.WriteHeader(header); err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			_, err = io.Copy(tarball, file)
+			return err
+		})
+}
+
+func untar(tarball, target string) error {
+	reader, err := os.Open(tarball)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	tarReader := tar.NewReader(reader)
+
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		path := filepath.Join(target, header.Name)
+		info := header.FileInfo()
+		if info.IsDir() {
+			if err = os.MkdirAll(path, info.Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+
+		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(file, tarReader)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func ungzip(source, target string) error {
+
+	reader, err := os.Open(source)
+
+	if err != nil {
+
+		return err
+
+	}
+
+	defer reader.Close()
+
+	archive, err := gzip.NewReader(reader)
+
+	if err != nil {
+
+		return err
+
+	}
+
+	defer archive.Close()
+
+	target = filepath.Join(target, archive.Name)
+
+	writer, err := os.Create(target)
+
+	if err != nil {
+
+		return err
+
+	}
+
+	defer writer.Close()
+
+	_, err = io.Copy(writer, archive)
+
+	return err
+
+}
+
+func gzipit(source, target string) error {
+
+	reader, err := os.Open(source)
+
+	if err != nil {
+
+		return err
+
+	}
+
+	filename := filepath.Base(source)
+
+	target = filepath.Join(target, fmt.Sprintf("%s.gz", filename))
+
+	writer, err := os.Create(target)
+
+	if err != nil {
+
+		return err
+
+	}
+
+	defer writer.Close()
+
+	archiver := gzip.NewWriter(writer)
+
+	archiver.Name = filename
+
+	defer archiver.Close()
+
+	_, err = io.Copy(archiver, reader)
+
+	return err
+
 }
 
 func docker_getconfig(library string, tag string) (map[string]string, error) {
@@ -111,6 +284,22 @@ func docker_getconfig(library string, tag string) (map[string]string, error) {
 	return m, nil
 }
 
+func RemoveDuplicatesFromSlice(s []string) []string {
+	m := make(map[string]bool)
+	for _, item := range s {
+		if _, ok := m[item]; ok {
+		} else {
+			m[item] = true
+		}
+	}
+
+	var result []string
+	for item, _ := range m {
+		result = append(result, item)
+	}
+	return result
+}
+
 func dockerpull(library string, tag string, path string) error {
 	resp, err := http.Get("https://auth.docker.io/token?service=registry.docker.io&scope=repository:" + library + ":pull")
 	if err != nil {
@@ -120,41 +309,86 @@ func dockerpull(library string, tag string, path string) error {
 	var result map[string]interface{}
 	json.NewDecoder(resp.Body).Decode(&result)
 	token := result["token"].(string)
+
 	req, err2 := http.NewRequest("GET", "https://registry-1.docker.io/v2/"+library+"/manifests/"+tag, nil)
+	req.Header.Add("Accept", "application/vnd.docker.distribution.manifest.v2+json")
 	req.Header.Add("Authorization", "Bearer "+string(token))
+
 	client := &http.Client{}
 	resp, err2 = client.Do(req)
 	if err2 != nil {
 		return fmt.Errorf("Failed retrieving blobs")
 	}
 
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("Failed retrieving blobs")
+	}
+
 	defer resp.Body.Close()
-	json.NewDecoder(resp.Body).Decode(&result)
-	blobs := result["fsLayers"].([]interface{})
+
+	schema := make(map[string]interface{})
+	json.Unmarshal(bodyBytes, &schema)
+	blobs := schema["layers"].([]interface{})
+	var gzblobs []string
 
 	for _, v := range blobs {
 		m := v.(map[string]interface{})
-		for _, o := range m {
-			url := "https://registry-1.docker.io/v2/" + library + "/blobs/" + o.(string)
-			req, err := http.NewRequest("GET", url, nil)
-			req.Header.Add("Authorization", "Bearer "+string(token))
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				return fmt.Errorf("Failed retrieving image blob: " + o.(string))
+		for k, o := range m {
+			if k == "digest" {
+				gzblobs = append(gzblobs, o.(string))
 			}
-			defer resp.Body.Close()
-			// Create the file
-			out, err := os.Create(path)
-			if err != nil {
-				return fmt.Errorf("Failed creating temporary image")
-			}
-			defer out.Close()
-
-			// Write the body to file
-			_, err = io.Copy(out, resp.Body)
 		}
 	}
+
+	dedupblobs := RemoveDuplicatesFromSlice(gzblobs)
+
+	for _, blob := range dedupblobs {
+		url := "https://registry-1.docker.io/v2/" + library + "/blobs/" + blob
+		req, err := http.NewRequest("GET", url, nil)
+		req.Header.Add("Authorization", "Bearer "+string(token))
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("Failed retrieving image blob: " + blob)
+		}
+		defer resp.Body.Close()
+		// Create the file
+		out, err := os.Create("/tmp/" + blob + ".gz")
+		if err != nil {
+			return fmt.Errorf("Failed creating temporary image")
+		}
+		defer out.Close()
+
+		// Write the body to file
+		_, err = io.Copy(out, resp.Body)
+
+		// Create the actual image from layers
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			os.Mkdir(path, os.ModePerm)
+		}
+		args := []string{"xvfz", "/tmp/" + blob + ".gz", "-C", path}
+
+		if err := exec.Command("tar", args...).Run(); err != nil {
+			return fmt.Errorf("error running tar:", err, args)
+		}
+
+		cleanerr := os.Remove("/tmp/" + blob + ".gz")
+		if cleanerr != nil {
+			return fmt.Errorf("Failed cleaning up", cleanerr)
+		}
+	}
+	cargs := []string{"cvfz", path + ".tar.gz", "-C", path, "."}
+	if err := exec.Command("tar", cargs...).Run(); err != nil {
+		return  fmt.Errorf("error running compress:", err, cargs)
+	}
+
+	cleanerr := os.RemoveAll(path)
+
+	if cleanerr != nil {
+		return fmt.Errorf("Failed cleaning up image dir", cleanerr)
+	}
+
 	return nil
 }
 
@@ -209,7 +443,7 @@ func (d *Driver) initializeContainer(cfg *drivers.TaskConfig, taskConfig TaskCon
 				library = name[1]
 			}
 
-			path := "/tmp/" + library + "-" + tag + "-" + uuid + ".gz"
+			path := "/tmp/" + library + "-" + tag + "-" + uuid
 
 			libtag := s[0]
 
@@ -220,9 +454,11 @@ func (d *Driver) initializeContainer(cfg *drivers.TaskConfig, taskConfig TaskCon
 			err := dockerpull(libtag, tag, path)
 
 			if err == nil {
-				img := config.Attribute{Name: "img", Type: "string", Value: path}
+				img := config.Attribute{Name: "img", Type: "string", Value: path + ".tar.gz"}
 				z.Attributes = append(z.Attributes, img)
-				d.logger.Info("taskConfig.Attributes", "driver_initialize_container", hclog.Fmt("%v+", z.Attributes))
+				d.logger.Info("driver_initialize_container", "docker_pull", hclog.Fmt("%v+", z.Attributes))
+			} else {
+				d.logger.Info("driver_initialize_container", "docker_pull failed", hclog.Fmt("%v+", err))
 			}
 
 			m, err := docker_getconfig(libtag, tag)
